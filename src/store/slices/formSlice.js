d7992d0d8caf1594d9a9be3a976597bbf2cbd6f7
Mirko Zlatunic <m.zlatunic@gmail.com>
2024-06-21 14:40:50 -0700

    first setup formSlice

--- src/store/slices/formSlice.js
+++ src/store/slices/formSlice.js
@@ -1,13 +1,20 @@
 import { createSlice } from "@reduxjs/toolkit";
 
-const fromSlice = createSlice({
+const formSlice = createSlice({
   name: "form",
   initialState: {
     name: "",
     cost: 0,
   },
   reducer: {
-    changeName(state, action) {},
-    changeCost(state, action) {},
+    changeName(state, action) {
+      state.name = action.payload;
+    },
+    changeCost(state, action) {
+      state.cost = action.payload;
+    },
   },
 });
+
+export const { changeName, changeCost } = formSlice.actions;
+export const formReducer = createSlice.reducer;
